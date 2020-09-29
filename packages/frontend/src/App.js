2242b9afa154941026a59423c38a3fb9a3c5ac62
Christian Mund <git@mundpropaganda.net>
2020-09-29 10:49:49 +0200

    feat: add dedicated api service (#1)
    * feat: add api service package
    * feat(crawler): upload data to api service instead of jsonbin
    * feat(frontend): fetch data from api service instead of jsonbin
    * fix(api): store `last_modified` as string
    * fix(frontend): add "accept" header to fetch request
    * fix(api): sort data by "date" descending

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -124,7 +124,9 @@ const App = () => {
 
   useEffect(() => {
     window
-      .fetch('https://jsonbin.mundpropaganda.net/kkkrist/coronastats-fl/')
+      .fetch('https://coronastats-fl.mundpropaganda.net/api/fl', {
+        headers: { accept: 'application/json' }
+      })
       .then(res => res.json())
       .then(data => {
         setError()
