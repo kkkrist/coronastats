fe8720f6859995349d2c3be4b7ff3fe084d0860b
Christian Mund <git@mundpropaganda.net>
2020-09-30 10:41:44 +0200

    feat(crawler): turn into multi-crawler framework, add crawler for "rz"

--- packages/crawler/lib/index.js
+++ packages/crawler/lib/index.js
@@ -1,7 +1,28 @@
+const fetch = require('node-fetch')
 const fl = require('./fl')
+const rz = require('./rz')
+
 require('dotenv').config()
 
-fl().then(
-  res => console.log(res.url, res.status, res.statusText),
-  console.error
-)
+Promise.all([fl(), rz()])
+  .then(data =>
+    Promise.all(
+      data.map(({ col, stats }) =>
+        fetch(`${process.env.API_URL}/${col}`, {
+          body: JSON.stringify(stats),
+          headers: {
+            Authorization: `token ${process.env.API_TOKEN}`,
+            'Content-Type': 'application/json'
+          },
+          method: 'POST'
+        })
+      )
+    )
+  )
+  .then(
+    responses =>
+      responses.forEach(res =>
+        console.log(res.url, res.status, res.statusText)
+      ),
+    console.error
+  )
---
+++ packages/crawler/lib/rz.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const jsdom = require('jsdom').JSDOM
+
+module.exports = () =>
+  new Promise((resolve, reject) => {
+    jsdom.fromURL('https://www.kreis-rz.de/Corona').then(dom => {
+      const str = dom.window.document.querySelector('h1').nextElementSibling
+        .textContent
+
+      const infectedMatch = str.match(/gesamt.*?([0-9]+)/)
+
+      const recoveredMatch = str.match(/genesen.*?([0-9]+)/)
+
+      const deathsMatch = str.match(/verstorben.*?([0-9]+)/)
+
+      const dateMatch = str.match(/([0-9]+)\.([0-9]+)\.([0-9]+)/)
+
+      if (!infectedMatch || !recoveredMatch || !deathsMatch || !dateMatch) {
+        return reject(new Error(`Couldn't parse string "${str}"`))
+      }
+
+      const entry = {
+        date: new Date(`${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}`),
+        deaths: Number(deathsMatch[1]),
+        infected: Number(infectedMatch[1]),
+        recovered: Number(recoveredMatch[1])
+      }
+
+      resolve({
+        col: 'rz',
+        stats: [
+          {
+            ...entry,
+            active: entry.infected - entry.recovered - entry.deaths
+          }
+        ]
+      })
+    }, reject)
+  })
