665b06a1d71e60e2501573df96d725fdfe4996cf
Christian Mund <git@mundpropaganda.net>
2020-10-01 10:24:42 +0200

    fix(crawler): handle crawlers independently

--- packages/crawler/lib/index.js
+++ packages/crawler/lib/index.js
@@ -5,25 +5,19 @@ const sl = require('./sl')
 
 require('dotenv').config()
 
-Promise.all([fl(), rz(), sl()])
-  .then(data =>
-    Promise.all(
-      data.map(({ col, stats }) =>
-        fetch(`${process.env.API_URL}/${col}`, {
-          body: JSON.stringify(stats),
-          headers: {
-            Authorization: `token ${process.env.API_TOKEN}`,
-            'Content-Type': 'application/json'
-          },
-          method: 'POST'
-        })
-      )
-    )
-  )
-  .then(
-    responses =>
-      responses.forEach(res =>
-        console.log(res.url, res.status, res.statusText)
-      ),
+const mapper = ({ col, stats }) =>
+  fetch(`${process.env.API_URL}/${col}`, {
+    body: JSON.stringify(stats),
+    headers: {
+      Authorization: `token ${process.env.API_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  }).then(
+    res => console.log(res.url, res.status, res.statusText),
     console.error
   )
+
+fl().then(mapper)
+rz().then(mapper)
+sl().then(mapper)
