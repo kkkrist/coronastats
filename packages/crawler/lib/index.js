a3b186957d6fac5243ce4329459e4def5525e346
Christian Mund <git@mundpropaganda.net>
2020-10-05 23:47:31 +0200

    feat(crawler): merge duplicate entries

--- packages/crawler/lib/index.js
+++ packages/crawler/lib/index.js
@@ -1,9 +1,4 @@
 const nano = require('nano')
-
-const fl = require('./fl')
-const rz = require('./rz')
-const sl = require('./sl')
-
 require('dotenv').config()
 
 const db = nano(process.env.COUCHDB_URI)
@@ -11,18 +6,41 @@ const db = nano(process.env.COUCHDB_URI)
 const upsert = stats =>
   db.get('_all_docs', { include_docs: true }).then(({ rows }) =>
     db.bulk({
-      docs: stats.map(stat => ({
-        ...(rows.find(
+      docs: stats.reduce((acc, stat) => {
+        const i = acc.findIndex(
+          ({ areacode, date }) =>
+            areacode === stat.areacode &&
+            date.toISOString() === stat.date.toISOString()
+        )
+
+        if (i > -1) {
+          acc[i] = {
+            ...acc[i],
+            ...stat,
+            last_modified: new Date().toISOString()
+          }
+          return acc
+        }
+
+        const row = rows.find(
           ({ doc }) =>
-            doc.areacode === stat.areacode && doc.date === stat.date.toISOString()
-        )?.doc || {}),
-        ...stat,
-        last_modified: new Date().toISOString()
-      }))
+            doc.areacode === stat.areacode &&
+            doc.date === stat.date.toISOString()
+        )
+
+        return [
+          ...acc,
+          {
+            ...(row ? row.doc : {}),
+            ...stat,
+            last_modified: new Date().toISOString()
+          }
+        ]
+      }, [])
     })
   )
 
-Promise.all([fl(), rz(), sl()])
+Promise.all([require('./fl')(), require('./rz')(), require('./sl')()])
   .then(data => upsert(data.flat()))
   .then(docs => {
     const errors = docs.filter(d => d.error)
