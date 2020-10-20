4c2cc071a03c7f52664dad9203641be6ea0f17d3
Christian Mund <git@mundpropaganda.net>
2020-10-20 10:32:58 +0200

    feat: add a service worker (#4)
    * feat(frontend): add rudimentary service worker
    
    * fix(frontend): link manifest, apple-touch-icon in html header
    
    * fix(frontend): use black-on-white icons
    
    * fix(frontend): use app container bg color as bg color in manifest
    
    * feat(frontend): move service worker to App component to be able to use notifications
    
    * fix(frontend): fix service worker setup
    
    * fix(frontend): fix notifications style

---
+++ packages/frontend/src/service-worker.js
@@ -0,0 +1,103 @@
+const registerValidSW = (swUrl, addNotification) => {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing
+
+        if (installingWorker == null) {
+          return
+        }
+
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              addNotification(
+                'Es ist ein Update verfügbar. Schließen Sie das Browser-Fenster/-Tab und laden Sie die App neu!'
+              )
+            } else {
+              addNotification('Die App ist nun offline verfügbar.')
+            }
+          }
+        }
+      }
+    })
+    .catch(error => {
+      addNotification(
+        `Fehler bei der Einrichtung des Offline-Modus: ${error}`,
+        'danger'
+      )
+    })
+}
+
+const checkValidServiceWorker = (swUrl, addNotification) => {
+  fetch(swUrl, {
+    headers: { 'Service-Worker': 'script' }
+  })
+    .then(response => {
+      const contentType = response.headers.get('content-type')
+
+      if (
+        response.status === 404 ||
+        (contentType != null && contentType.indexOf('javascript') === -1)
+      ) {
+        navigator.serviceWorker.ready.then(registration => {
+          registration.unregister().then(() => {
+            window.location.reload()
+          })
+        })
+      } else {
+        registerValidSW(swUrl, addNotification)
+      }
+    })
+    .catch(() => {
+      addNotification(
+        'Es besteht keine Internetverbindung. Die App befindet sich im Offline-Modus.',
+        'warning'
+      )
+    })
+}
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/
+    )
+)
+
+export const register = addNotification => {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
+
+    if (publicUrl.origin !== window.location.origin) {
+      return
+    }
+
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl, addNotification)
+
+        navigator.serviceWorker.ready.then(() => {
+          console.log('Service worker is active')
+        })
+      } else {
+        registerValidSW(swUrl, addNotification)
+      }
+    })
+  }
+}
+
+export const unregister = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then(registration => {
+        registration.unregister()
+      })
+      .catch(error => {
+        console.error(error.message)
+      })
+  }
+}
