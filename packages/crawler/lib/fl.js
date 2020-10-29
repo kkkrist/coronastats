898580ea1b80fed759e194190d4e30ce267df20f
Christian Mund <git@mundpropaganda.net>
2020-10-29 16:16:13 +0100

    feat(crawler): re-write "fl" crawler once more (are those people fat-fingering the data in on a typewriter?!)

--- packages/crawler/lib/fl.js
+++ packages/crawler/lib/fl.js
@@ -2,94 +2,83 @@
 
 const jsdom = require('jsdom').JSDOM
 
+const rDate = /([0-9]+)\.([0-9]+)\.([0-9]+)/
+const rDeaths = /([0-9]+)[\D]+Verst(?:or|ro)?ben/i
+const rInfected = /([0-9]+)[\D]+Infizierte/
+const rQuarantined = /([0-9]+)[\D]+ (?<!Lehrer )in Qua?rantäne/
+const rRecovered = /([0-9]+)[\D]+gen?en?sen/
+
+const getMatch = (el, regex, isOptional) => {
+  let match
+  let nextEl = el.previousSibling
+
+  while (nextEl && !match) {
+    match = nextEl.textContent.match(regex)
+    nextEl = nextEl.previousSibling || nextEl.parentNode
+  }
+
+  if (!isOptional && !match) {
+    throw new Error(`Couldn't find ${regex.toString()} in "${el.textContent}"!`)
+  }
+
+  return match
+}
+
+const getRecord = el => {
+  const dateMatch = getMatch(el, rDate)
+  const deathsMatch = getMatch(el, rDeaths)
+  const infectedMatch = getMatch(el, rInfected)
+  const quarantinedMatch = getMatch(el, rQuarantined)
+  const recoveredMatch = getMatch(el, rRecovered)
+
+  const record = {
+    areacode: 'fl',
+    date: new Date(
+      `20${dateMatch[3].slice(-2)}-${dateMatch[2]}-${dateMatch[1]}`
+    ).toISOString(),
+    deaths: Number(deathsMatch[1]),
+    infected: Number(infectedMatch[1]),
+    quarantined: Number(quarantinedMatch[1]),
+    recovered: Number(recoveredMatch[1])
+  }
+
+  return {
+    ...record,
+    active: record.infected - record.recovered - record.deaths
+  }
+}
+
+const reducer = (acc, el) => {
+  if (
+    el.childElementCount > 0 &&
+    ![...el.childNodes].every(node => node.tagName === 'SPAN')
+  ) {
+    return [...el.childNodes].reduce(reducer, acc)
+  }
+
+  const str = el.textContent
+
+  if (
+    rInfected.test(str) &&
+    rRecovered.test(str) &&
+    rQuarantined.test(str) &&
+    rDeaths.test(str)
+  ) {
+    return [...acc, getRecord(el)]
+  }
+
+  return acc
+}
+
 module.exports = () =>
-  new Promise((resolve, reject) => {
-    jsdom
-      .fromURL(
-        'https://www.flensburg.de/Startseite/Informationen-zum-Coronavirus.php?object=tx,2306.5&ModID=7&FID=2306.20374.1'
-      )
-      .then(
-        dom =>
-          resolve(
-            [...dom.window.document.querySelectorAll('h2')]
-              .filter(el => /^([0-9]+)\.([0-9]+)\.2[0-9]$/.test(el.textContent))
-              .reduce((data, date) => {
-                let content = ''
-                let nextEl = date.nextElementSibling
-
-                while (nextEl && nextEl.tagName !== 'H2') {
-                  content = `${nextEl.textContent}\n${content}`
-                  nextEl = nextEl.nextElementSibling
-                }
-
-                const infectedMatch = content.match(
-                  /([0-9]+)\*? nachweislich Infizierte/
-                )
-
-                const recoveredMatch = content.match(/([0-9]+)\*?.*genesen/)
-
-                const quarantinedMatch = content.match(
-                  /([0-9]+)\*? Verdachtsfälle in Qua?rantäne/
-                )
-
-                const deathsMatch = content.match(/([0-9]+)\*? Verstorbene/)
-
-                const dateMatch = date.textContent.match(
-                  /([0-9]+)\.([0-9]+)\.([0-9]+)/
-                )
-
-                const timeMatch = content.match(/Stand ([0-9]+)\.([0-9]+) Uhr/)
-
-                if (!infectedMatch) {
-                  throw new Error(
-                    `Couldn't parse infected of ${date.textContent}`
-                  )
-                }
-
-                if (!recoveredMatch) {
-                  throw new Error(
-                    `Couldn't parse recovered of ${date.textContent}`
-                  )
-                }
-
-                if (!quarantinedMatch) {
-                  throw new Error(
-                    `Couldn't parse quarantined of ${date.textContent}`
-                  )
-                }
-
-                if (!deathsMatch) {
-                  throw new Error(
-                    `Couldn't parse deaths of ${date.textContent}`
-                  )
-                }
-
-                if (!dateMatch) {
-                  throw new Error(`Couldn't parse date of ${date.textContent}`)
-                }
-
-                const entry = {
-                  areacode: 'fl',
-                  date: new Date(
-                    `20${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}${
-                      timeMatch ? ` ${timeMatch[1]}:${timeMatch[2]}` : ''
-                    }`
-                  ),
-                  deaths: Number(deathsMatch[1]),
-                  infected: Number(infectedMatch[1]),
-                  quarantined: Number(quarantinedMatch[1]),
-                  recovered: Number(recoveredMatch[1])
-                }
-
-                return [
-                  ...data,
-                  {
-                    ...entry,
-                    active: entry.infected - entry.recovered - entry.deaths
-                  }
-                ]
-              }, [])
-          ),
-        reject
-      )
-  })
+  jsdom
+    .fromURL(
+      'https://www.flensburg.de/Startseite/Informationen-zum-Coronavirus.php?object=tx,2306.5&ModID=7&FID=2306.20374.1'
+    )
+    .then(dom =>
+      [
+        ...dom.window.document.querySelector(
+          '.einleitung + div > div > .toggler_container'
+        ).childNodes
+      ].reduce(reducer, [])
+    )
