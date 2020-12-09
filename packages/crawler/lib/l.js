c4940e81418c365b3741c86dbea5ec545c7107ea
Christian Mund <git@mundpropaganda.net>
2020-12-09 09:57:48 +0100

    fix(crawler/l): compare with last entry, don't same if numbers match up

--- packages/crawler/lib/l.js
+++ packages/crawler/lib/l.js
@@ -2,67 +2,85 @@
 
 const jsdom = require('jsdom').JSDOM
 const fetchOptions = require('./fetch-options.json')
+const fetch = require('node-fetch')
 
 module.exports = () =>
-  jsdom
-    .fromURL(
+  Promise.all([
+    jsdom.fromURL(
       'https://www.leipzig.de/jugend-familie-und-soziales/gesundheit/neuartiges-coronavirus-2019-n-cov/',
       {
         userAgent: fetchOptions.headers['user-agent']
       }
-    )
-    .then(
-      dom => {
-        const content =
-          dom.window.document.querySelector('h2').nextElementSibling
-            .textContent +
-          '\n' +
-          dom.window.document.querySelector('div.schlaglichter-container')
-            .textContent
-
-        const activeMatch = content.match(/aktive Fälle. ([0-9.]+)/)
-
-        const dateMatch = content.match(/Stand ([0-9]+)\.([0-9]+)\.([0-9]+)/)
-
-        const deathsMatch = content.match(/bisher ([0-9]+) Todesfälle/)
-
-        const infectedMatch = content.match(
-          /positiv Getestete. bisher insgesamt ([0-9.]+)/
-        )
-
-        const quarantinedMatch = content.match(
-          /in häuslicher Quarantäne. [0-9.]+ positiv Getestete, ([0-9.]+) Kontaktpersonen/
-        )
-
-        ;[
-          activeMatch,
-          dateMatch,
-          deathsMatch,
-          infectedMatch,
-          quarantinedMatch
-        ].forEach(match => {
-          if (!match) {
-            throw new Error(`Couldn't parse ${match}`)
-          }
-        })
-
-        const entry = {
-          areacode: 'l',
-          active: Number(activeMatch[1].replace('.', '')),
-          date: `${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}T00:00:00.000Z`,
-          deaths: Number(deathsMatch[1]),
-          infected: Number(infectedMatch[1].replace('.', '')),
-          quarantined: Number(quarantinedMatch[1].replace('.', ''))
-        }
-
-        return [
-          {
-            ...entry,
-            recovered: entry.infected - entry.active - entry.deaths
-          }
-        ]
-      },
-      error => {
-        throw error
+    ),
+    fetch(
+      'https://api.mundpropaganda.net/coronastats/_design/areacode/_view/l?descending=true&limit=1',
+      fetchOptions
+    ).then(res => res.json())
+  ]).then(
+    ([
+      dom,
+      {
+        rows: [lastEntry]
       }
-    )
+    ]) => {
+      const content =
+        dom.window.document.querySelector('h2').nextElementSibling.textContent +
+        '\n' +
+        dom.window.document.querySelector('div.schlaglichter-container')
+          .textContent
+
+      const activeMatch = content.match(/aktive Fälle. ([0-9.]+)/)
+
+      const dateMatch = content.match(/Stand ([0-9]+)\.([0-9]+)\.([0-9]+)/)
+
+      const deathsMatch = content.match(/bisher ([0-9]+) Todesfälle/)
+
+      const infectedMatch = content.match(
+        /positiv Getestete. bisher insgesamt ([0-9.]+)/
+      )
+
+      const quarantinedMatch = content.match(
+        /in häuslicher Quarantäne. [0-9.]+ positiv Getestete, ([0-9.]+) Kontaktpersonen/
+      )
+
+      ;[
+        activeMatch,
+        dateMatch,
+        deathsMatch,
+        infectedMatch,
+        quarantinedMatch
+      ].forEach(match => {
+        if (!match) {
+          throw new Error(`Couldn't parse ${match}`)
+        }
+      })
+
+      const entry = {
+        areacode: 'l',
+        active: Number(activeMatch[1].replace('.', '')),
+        date: `${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}T00:00:00.000Z`,
+        deaths: Number(deathsMatch[1]),
+        infected: Number(infectedMatch[1].replace('.', '')),
+        quarantined: Number(quarantinedMatch[1].replace('.', ''))
+      }
+
+      if (
+        entry.active === lastEntry.value.active &&
+        entry.deaths === lastEntry.value.deaths &&
+        entry.infected === lastEntry.value.infected &&
+        entry.quarantined === lastEntry.value.quarantined
+      ) {
+        return []
+      }
+
+      return [
+        {
+          ...entry,
+          recovered: entry.infected - entry.active - entry.deaths
+        }
+      ]
+    },
+    error => {
+      throw error
+    }
+  )
