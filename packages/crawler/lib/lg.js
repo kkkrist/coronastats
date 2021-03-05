f56fe61cea7f1c75133ea05794dd8cd474251e3b
Christian Mund <git@mundpropaganda.net>
2021-03-05 17:55:30 +0100

    fix(crawler/lg): adapt to new api

--- packages/crawler/lib/lg.js
+++ packages/crawler/lib/lg.js
@@ -1,73 +1,52 @@
 'use strict'
 
-const jsdom = require('jsdom').JSDOM
+const fetch = require('node-fetch')
 const fetchOptions = require('./fetch-options.json')
 
 module.exports = () =>
-  jsdom
-    .fromURL('https://corona.landkreis-lueneburg.de/aktuelle-situation/', {
-      userAgent: fetchOptions.headers['user-agent']
-    })
+  fetch(
+    'https://corona.landkreis-lueneburg.de/wp-admin/admin-ajax.php?action=get_wdtable&table_id=15',
+    {
+      ...fetchOptions,
+      body:
+        'draw=1&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=wdt_ID&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=meldedatum&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=7tagesinzidenzpro100000einwohner&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=aktiveflle&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=neueflle&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=fllegesamt&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=6&columns%5B6%5D%5Bname%5D=genesene&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=7&columns%5B7%5D%5Bname%5D=todesflle&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B8%5D%5Bdata%5D=8&columns%5B8%5D%5Bname%5D=neuetodesflle&columns%5B8%5D%5Bsearchable%5D=true&columns%5B8%5D%5Borderable%5D=true&columns%5B8%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B8%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B9%5D%5Bdata%5D=9&columns%5B9%5D%5Bname%5D=bemerkungen&columns%5B9%5D%5Bsearchable%5D=true&columns%5B9%5D%5Borderable%5D=true&columns%5B9%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B9%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=1&order%5B0%5D%5Bdir%5D=desc&start=0&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&wdtNonce=7e3ecd69fa',
+      headers: {
+        ...fetchOptions.headers,
+        accept: 'application/json',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      method: 'POST'
+    }
+  )
+    .then(res => res.json())
     .then(
-      dom =>
-        [...dom.window.document.querySelectorAll('tr')]
-          .slice(2)
-          .reduce((acc, row) => {
-            if (row.children.length < 6) {
-              return acc
-            }
+      ({ data }) =>
+        data.reduce((acc, row) => {
+          const dateMatch = row[1].match(/([0-9]+)\.([0-9]+)\.([0-9]+)/)
 
-            const dateMatch = row.children[0].textContent.match(
-              /([0-9]+)\.([0-9]+)\.([0-9]+)/
+          if (!dateMatch) {
+            throw new Error(
+              `Couldn't parse date string "${row.children[0].textContent}"`
             )
+          }
 
-            const infectedMatch = row.children[3].textContent.match(/([0-9.]+)/)
+          const entry = {
+            areacode: 'lg',
+            date: `${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}T00:00:00.000Z`,
+            deaths: Number(row[7].replace('.', '')),
+            infected: Number(row[5].replace('.', '')),
+            quarantined: null,
+            recovered: Number(row[6].replace('.', ''))
+          }
 
-            const recoveredMatch = row.children[4].textContent.match(
-              /([0-9.]+)/
-            )
-
-            const deathsMatch = row.children[5].textContent.match(/([0-9.]+)/)
-
-            if (!recoveredMatch) {
-              return acc
+          return [
+            ...acc,
+            {
+              ...entry,
+              active: entry.infected - entry.recovered - entry.deaths
             }
-
-            if (!dateMatch) {
-              throw new Error(
-                `Couldn't parse date string "${row.children[0].textContent}"`
-              )
-            }
-
-            if (!infectedMatch) {
-              throw new Error(
-                `Couldn't parse infected string "${row.children[3].textContent}"`
-              )
-            }
-
-            if (!deathsMatch) {
-              throw new Error(
-                `Couldn't parse deaths string "${row.children[4].textContent}"`
-              )
-            }
-
-            const entry = {
-              areacode: 'lg',
-              date: `${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}T00:00:00.000Z`,
-              deaths: Number(deathsMatch[1].replace('.', '')),
-              infected: Number(infectedMatch[1].replace('.', '')),
-              quarantined: null,
-              recovered: Number(recoveredMatch[1].replace('.', ''))
-            }
-
-            return [
-              ...acc,
-              {
-                ...entry,
-                active: entry.infected - entry.recovered - entry.deaths
-              }
-            ]
-          }, []),
+          ]
+        }, []),
       error => {
         throw error
       }
