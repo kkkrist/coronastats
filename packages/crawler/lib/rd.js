73f122380e66cef778a8c5d41a15eed4f50fbd85
Christian Mund <git@mundpropaganda.net>
2022-02-23 14:52:09 +0100

    fix(crawler/rd): don't push entry if numbers don't differ from last

--- packages/crawler/lib/rd.js
+++ packages/crawler/lib/rd.js
@@ -6,49 +6,71 @@ const fetchOptions = require('./fetch-options.json')
 module.exports = () =>
   Promise.all([
     fetch('https://covid19dashboardrdeck.aco/daten/update.js', fetchOptions),
-    fetch('https://covid19dashboardrdeck.aco/daten/dash.js', fetchOptions)
+    fetch('https://covid19dashboardrdeck.aco/daten/dash.js', fetchOptions),
+    fetch(
+      'https://api.mundpropaganda.net/coronastats/_design/areacode/_view/rd?descending=true&limit=1',
+      fetchOptions
+    )
   ])
-    .then(responses => Promise.all(responses.map(res => res.text())))
-    .then(async texts => {
-      const text = texts.join('')
+    .then(([update, dash, lastEntry]) =>
+      Promise.all([update.text(), dash.text(), lastEntry.json()])
+    )
+    .then(
+      async ([
+        update,
+        dash,
+        {
+          rows: [lastEntry]
+        }
+      ]) => {
+        const text = [update, dash].join('')
 
-      const matches = {
-        activeMatch: text.match(/todayActivInfected\s?=\s?'(\d+)'/),
-        dateMatch: text.match(
-          /lastUpdate\s?=\s?'(\d+)\.(\d+)\.(\d+)\s(\d+):(\d+):(\d+)/
-        ),
-        deathsMatch: text.match(/todayDeath\s?=\s?'(\d+)'/),
-        infectedMatch: text.match(/todaySumInfected\s?=\s?'(\d+)'/),
-        recoveredMatch: text.match(/todayHealth\s?=\s?'(\d+)'/)
+        const matches = {
+          activeMatch: text.match(/todayActivInfected\s?=\s?'(\d+)'/),
+          dateMatch: text.match(
+            /lastUpdate\s?=\s?'(\d+)\.(\d+)\.(\d+)\s(\d+):(\d+):(\d+)/
+          ),
+          deathsMatch: text.match(/todayDeath\s?=\s?'(\d+)'/),
+          infectedMatch: text.match(/todaySumInfected\s?=\s?'(\d+)'/),
+          recoveredMatch: text.match(/todayHealth\s?=\s?'(\d+)'/)
+        }
+
+        const errors = Object.entries(matches).filter(([key, value]) => !value)
+
+        if (errors.length > 0) {
+          throw new Error(
+            `No match for: ${errors.map(([key]) => key).join(', ')}`
+          )
+        }
+
+        const {
+          activeMatch,
+          dateMatch,
+          deathsMatch,
+          infectedMatch,
+          recoveredMatch
+        } = matches
+
+        const entry = {
+          areacode: 'rd',
+          active: Number(activeMatch[1]),
+          date: new Date(
+            `${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}T00:00:00.000Z`
+          ).toISOString(),
+          deaths: Number(deathsMatch[1]),
+          infected: Number(infectedMatch[1]),
+          quarantined: null,
+          recovered: Number(recoveredMatch[1])
+        }
+
+        if (
+          entry.active === lastEntry.value.active &&
+          entry.recovered === lastEntry.value.recovered &&
+          entry.deaths === lastEntry.value.deaths
+        ) {
+          return []
+        }
+
+        return [entry]
       }
-
-      const errors = Object.entries(matches).filter(([key, value]) => !value)
-
-      if (errors.length > 0) {
-        throw new Error(
-          `No match for: ${errors.map(([key]) => key).join(', ')}`
-        )
-      }
-
-      const {
-        activeMatch,
-        dateMatch,
-        deathsMatch,
-        infectedMatch,
-        recoveredMatch
-      } = matches
-
-      const entry = {
-        areacode: 'rd',
-        active: Number(activeMatch[1]),
-        date: new Date(
-          `${dateMatch[3]}-${dateMatch[2]}-${dateMatch[1]}T00:00:00.000Z`
-        ).toISOString(),
-        deaths: Number(deathsMatch[1]),
-        infected: Number(infectedMatch[1]),
-        quarantined: null,
-        recovered: Number(recoveredMatch[1])
-      }
-
-      return [entry]
-    })
+    )
